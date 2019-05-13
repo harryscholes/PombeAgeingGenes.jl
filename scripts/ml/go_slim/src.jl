fbc035e6670644560a748434a87ff49065619ec1
harryscholes <harryscholes@gmail.com>
2019-05-13 11:11:04 +0100

    Add multiple repeats of CV

--- scripts/ml/go_slim/src.jl
+++ scripts/ml/go_slim/src.jl
@@ -9,6 +9,13 @@ end
 
 @everywhere using DecisionTree
 
+function setupdir(features)
+    dir = joinpath(ENV["POMBEAGEINGGENES"], "Scripts", "ml", "go_slim",
+                   "RandomForestClassifier", features)
+    isdir(dir) || mkpath(dir)
+    dir
+end
+
 function rfc(Xtrain, ytrain, Xtest; kwargs...)
     model = DecisionTree.fit!(RandomForestClassifier(; n_trees=100, kwargs...), Xtrain, ytrain)
     ŷ = DecisionTree.predict_proba(model, Xtest)[:,2]
@@ -23,19 +30,29 @@ function cv(X, y, grid)
     end
 end
 
-function cvgoslim(X, Y, goterms)
+function cvgoterms(X, Y, goterms; dir, runnumber=0)
     for i = 1:size(Y,1)
-        cvgoslim(X, Y, goterms, i)
+        goterm = string(goterms[i])
+        @show goterm
+        y = [j == 1 for j = Y[i,:]]
+        Random.seed!(runnumber+i)
+        cvgoterm(X, y, goterm; dir=dir)
     end
 end
 
-function cvgoslim(X, Y, goterms, i)
-    goterm = goterms[i]
-    @show goterm
-    fname = "$dir/$(goterm)"
-    y = [i == 1 for i = Y[i,:]]
-    Random.seed!(i)
+function cvgoterm(X, y, goterm; dir)
+    fname = joinpath(dir, goterm)
     ŷs, ys, p, pr = cv(X, y, grid)
-    savefig(plot(pr, legend=:topright), fname * ".pdf")
-    writecvresults(fname * ".json", ŷs, ys)
+    savefig(plot(pr, legend=:topright), fname*".pdf")
+    writecvresults(fname*".json", ŷs, ys)
+end
+
+repeats(nrepeats::Integer; dir::AbstractString) = repeats(1, nrepeats; dir=dir)
+
+function repeats(start::Integer, stop::Integer; dir::AbstractString)
+    for i = start:stop
+        dir_i = joinpath(dir, string(i))
+        isdir(dir_i) || mkpath(dir_i)
+        cvgoterms(X, Y, goterms, dir=dir_i; runnumber=i)
+    end
 end
