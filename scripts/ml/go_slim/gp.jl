4bf710b8aa77fcb7242d686067d4f497bfd16ad5
harryscholes <harryscholes@gmail.com>
2019-07-09 11:57:32 +0100

    Add trigitised features

--- scripts/ml/go_slim/gp.jl
+++ scripts/ml/go_slim/gp.jl
@@ -8,7 +8,9 @@ include("src.jl")
 dir = "$(ENV["POMBEAGEINGGENES"])/Scripts/ml/go_slim/RandomForestClassifier/gp"
 isdir(dir) || mkpath(dir)
 
-X, Y, goterms = load(ML, Matrix)
+X, Y, goterms = load(ML, growthphenotypes=false, Matrix)
+
+X, Y, goterms = load(ML, growthphenotypes=false, trigitised=true, Matrix)
 
 const grid = Dict(
     :n_subfeatures => [floor(sqrt(size(X,1))), 10, 25, 50],
